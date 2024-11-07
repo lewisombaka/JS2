994903b5c33e1eeed5ac263b28b9fbea134bf21c
lewisombaka <lewisochieng99@gmail.com>
2024-11-07 08:28:25 +0300

    Numbers

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 // console.log("I am!");
 
 // string
-const myVariable = "Mathematics";
+// const myVariable = "Mathematics";
 
-console.log(myVariable.length)
-console.log(myVariable.charAt(3))
-console.log(myVariable.indexOf("t"))
-console.log(myVariable.slice("5"))
-console.log(myVariable.slice("5,6"))
-console.log(myVariable.toUpperCase())
-console.log(myVariable.includes("div"))
+// console.log(myVariable.length)
+// console.log(myVariable.charAt(3))
+// console.log(myVariable.indexOf("t"))
+// console.log(myVariable.slice("5"))
+// console.log(myVariable.slice("5,6"))
+// console.log(myVariable.toUpperCase())
+// console.log(myVariable.includes("div"))
+
+
+// Number
+
+const myNumber = 42;
+const myFloat = 42.0;
+const myString = "42";
+
+console.log(myNumber === myFloat);
+console.log(myNumber === myString);
+
+const reverbedString = Number(myString);
+
+console.log(typeof(reverbedString));
+console.log(Number.isInteger(reverbedString));
+console.log(Number.parseFloat(myFloat));
+console.log(myFloat.toFixed(2));
+//pasre int
+// to string
