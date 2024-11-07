1a679fe423a52b04858f8226b4990d6897136802
lewisombaka <lewisochieng99@gmail.com>
2024-11-07 08:18:19 +0300

    string methods

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("I am!");
+// console.log("I am!");
+
+// string
+const myVariable = "Mathematics";
+
+console.log(myVariable.length)
+console.log(myVariable.charAt(3))
+console.log(myVariable.indexOf("t"))
+console.log(myVariable.slice("5"))
+console.log(myVariable.slice("5,6"))
+console.log(myVariable.toUpperCase())
+console.log(myVariable.includes("div"))
